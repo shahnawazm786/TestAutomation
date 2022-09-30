fbac04da74ca8af4cf38dbc718237db264784a8c
Mohammad Shahnawaz <msnazia2012@gmail.com>
2022-09-30 23:30:06 +0530

    utilities

--- src/test/java/com/steps/stepdef.java
+++ src/test/java/com/steps/stepdef.java
@@ -109,6 +109,6 @@ public class stepdef {
 	@And("select the first row of table")
 	public void select_the_first_row_of_table() {
 		driver.switchTo().defaultContent();
-		driver.findElement(By.cssSelector("table[id='resultTable']>tbody>tr:nth-child(1)>td:nth-child(1)")).click();
+		driver.findElement(By.cssSelector("table[id='resultTable']>tbody>tr:nth-child(1)>td:nth-child(1)>input")).click();
 	}
 }
